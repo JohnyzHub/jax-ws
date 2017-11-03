2d32063de2dfd47245b34d01fe3b5e51bded0c41
johnyshaik <jbshaik82@gmail.com>
2017-11-02 22:44:23 -0400

    JAX-WS:SOAP:DOCUMENT: Client - Service Stub
    	Client - Service Stub

---
+++ MyCalculatorClient/src/main/java/com/soap/document/client/ws/ObjectFactory.java
@@ -0,0 +1,107 @@
+
+package com.soap.document.client.ws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.soap.document.service package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Substract_QNAME = new QName("http://service.document.soap.com/", "substract");
+    private final static QName _Addition_QNAME = new QName("http://service.document.soap.com/", "addition");
+    private final static QName _SubstractResponse_QNAME = new QName("http://service.document.soap.com/", "substractResponse");
+    private final static QName _AdditionResponse_QNAME = new QName("http://service.document.soap.com/", "additionResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.soap.document.service
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link SubstractResponse }
+     * 
+     */
+    public SubstractResponse createSubstractResponse() {
+        return new SubstractResponse();
+    }
+
+    /**
+     * Create an instance of {@link AdditionResponse }
+     * 
+     */
+    public AdditionResponse createAdditionResponse() {
+        return new AdditionResponse();
+    }
+
+    /**
+     * Create an instance of {@link Substract }
+     * 
+     */
+    public Substract createSubstract() {
+        return new Substract();
+    }
+
+    /**
+     * Create an instance of {@link Addition }
+     * 
+     */
+    public Addition createAddition() {
+        return new Addition();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Substract }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.document.soap.com/", name = "substract")
+    public JAXBElement<Substract> createSubstract(Substract value) {
+        return new JAXBElement<Substract>(_Substract_QNAME, Substract.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Addition }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.document.soap.com/", name = "addition")
+    public JAXBElement<Addition> createAddition(Addition value) {
+        return new JAXBElement<Addition>(_Addition_QNAME, Addition.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link SubstractResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.document.soap.com/", name = "substractResponse")
+    public JAXBElement<SubstractResponse> createSubstractResponse(SubstractResponse value) {
+        return new JAXBElement<SubstractResponse>(_SubstractResponse_QNAME, SubstractResponse.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link AdditionResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://service.document.soap.com/", name = "additionResponse")
+    public JAXBElement<AdditionResponse> createAdditionResponse(AdditionResponse value) {
+        return new JAXBElement<AdditionResponse>(_AdditionResponse_QNAME, AdditionResponse.class, null, value);
+    }
+
+}
---
+++ MyCalculatorClient/src/main/java/com/soap/document/client/ws/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.document.soap.com/")
+package com.soap.document.client.ws;
